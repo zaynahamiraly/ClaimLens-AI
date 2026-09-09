2a2e7cf5c00e7489dca76cd4b3b5db2b94203cd0
zaynahamiraly <233393602+zaynahamiraly@users.noreply.github.com>
2026-09-09 15:05:47 +0400

    fix: bundle OCR image decoders

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,8 +6,13 @@ const nextConfig: NextConfig = {
     "/claims/*": [
       "./node_modules/tesseract.js/**/*",
       "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/idb-keyval/**/*",
+      "./node_modules/is-url/**/*",
       "./node_modules/wasm-feature-detect/**/*",
       "./node_modules/node-fetch/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/zlibjs/**/*",
     ],
   },
   experimental: {
